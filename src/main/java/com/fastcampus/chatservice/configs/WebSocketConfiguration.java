cc0b635b813526a661a461f11a6123ab81c19920
kwihyeon <imleo322@gmail.com>
2025-09-22 15:09:15 +0900

    feat : 첫 커밋

---
+++ src/main/java/com/fastcampus/chatservice/configs/WebSocketConfiguration.java
@@ -0,0 +1,21 @@
+package com.fastcampus.chatservice.configs;
+
+import com.fastcampus.chatservice.handlers.WebSocketChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket
+@RequiredArgsConstructor
+@Configuration
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    final WebSocketChatHandler webSocketChatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatHandler, "/ws/chats");
+    }
+}
